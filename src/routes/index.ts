9bf3c1f016646bee4ffd67285d4efa7efba09bf9
Rian Gabriel <riangabr2711@hotmail.com>
2023-07-07 22:47:47 -0300

    Added CreatePost

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import { userRoutes } from "./user.routes";
 import { sesssionRoutes } from "./sesssion.routes";
+import { postRoutes } from "./post.routes";
 
 const router = Router();
 
 router.use("/users", userRoutes);
 router.use("/sessions", sesssionRoutes);
+router.use("/posts", postRoutes);
 
 export { router };
--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -1,11 +1,18 @@
 // inversão de dependencia
+import { IPostsRepositories } from "@modules/posts/iRepositories/IPostsRespositories";
 import "./providers";
 
 import { IUsersRepositories } from "@modules/users/iRepositories/IUsersRepositories";
 import { UserRepository } from "@modules/users/repositories/UserRepository";
 import { container } from "tsyringe";
+import { PostRepository } from "@modules/posts/repositories/PostRepository";
 
 container.registerSingleton<IUsersRepositories>(
   "UserRepository",
   UserRepository
+);
+
+container.registerSingleton<IPostsRepositories>(
+  "PostRepository",
+  PostRepository
 );
