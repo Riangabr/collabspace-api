f9fb873d51652a893242ba408dc8ee3d5bd3f263
Rian Gabriel <riangabr2711@hotmail.com>
2023-07-12 20:53:56 -0300

    Added DeletePost Route

--- src/routes/post.routes.ts
+++ src/routes/post.routes.ts
@@ -5,6 +5,7 @@ import { CreatePostController } from "@modules/posts/useCases/createPost/createP
 import { authentication } from "src/middlewares/authentication";
 import { ListAllPostsController } from "@modules/posts/useCases/listAllPosts/listAllPostsController";
 import { UpdatePostController } from "@modules/posts/useCases/updatePost/updatePostController";
+import { DeletePostController } from "@modules/posts/useCases/deletePost/deletePostController";
 
 const postRoutes = Router();
 
@@ -14,5 +15,6 @@ postRoutes.use(authentication);
 
 postRoutes.post("/", new CreatePostController().handle);
 postRoutes.put("/:id", new UpdatePostController().handle);
+postRoutes.delete("/:id", new DeletePostController().handle);
 
 export { postRoutes };
