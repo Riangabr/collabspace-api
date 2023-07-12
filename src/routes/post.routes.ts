96186977901082855bc7589ced4762f9fa0d9b90
Rian Gabriel <riangabr2711@hotmail.com>
2023-07-11 22:37:40 -0300

    Added UpdatePosts

--- src/routes/post.routes.ts
+++ src/routes/post.routes.ts
@@ -3,7 +3,8 @@ import { Router } from "express";
 import { CreatePostController } from "@modules/posts/useCases/createPost/createPostController";
 
 import { authentication } from "src/middlewares/authentication";
-import { ListAllPostsController } from "@modules/posts/listAllPosts/listAllPostsController";
+import { ListAllPostsController } from "@modules/posts/useCases/listAllPosts/listAllPostsController";
+import { UpdatePostController } from "@modules/posts/useCases/updatePost/updatePostController";
 
 const postRoutes = Router();
 
@@ -12,5 +13,6 @@ postRoutes.get("/", new ListAllPostsController().handle);
 postRoutes.use(authentication);
 
 postRoutes.post("/", new CreatePostController().handle);
+postRoutes.put("/:id", new UpdatePostController().handle);
 
 export { postRoutes };
