10529a6756ed21b7a4282a857f04e081f98cb8fe
Rian Gabriel <riangabr2711@hotmail.com>
2023-07-21 21:42:17 -0300

    Added initialized friends module and createFriend

---
+++ src/enums/postVisibility.ts
@@ -0,0 +1,5 @@
+export enum EnumPostVisibility {
+  global = 1,
+  friends = 2,
+  private = 3,
+}
---
+++ src/enums/reactionEntityType.ts
@@ -0,0 +1,9 @@
+/* eslint-disable no-unused-vars */
+export enum EnumReactionTypes {
+  liked = 1, // Gostuei
+  loved = 2, // Não gostuei, asmei
+  laungh = 3, // HaHa
+  angry = 4, // Grr
+  sad = 5, //
+  notKidding = 6, // Truaisti
+}
